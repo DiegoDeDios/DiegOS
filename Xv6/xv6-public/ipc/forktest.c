c1745c196c2d0ab892f6408ff5ed7418a8dd5eda
Diego A <diegomzdedios@gmail.com>
2019-03-20 03:13:30 +0000

    practica5

--- Xv6/xv6-public/ipc/forktest.c
+++ Xv6/xv6-public/ipc/forktest.c
@@ -1,20 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
-int main(void)
-{
+#include <stdio.h> include <stdlib.h> include <errno.h> include <unistd.h> include <sys/types.h> include <sys/wait.h>
+int main(void) {
     pid_t pid;
     int rv;
-
     switch(pid = fork()) {
     case -1:
-        perror("fork");  /* something went wrong */
-        exit(1);         /* parent exits */
-
+        perror("fork"); /* something went wrong */
+        exit(1); /* parent exits */
     case 0:
         printf(" CHILD: This is the child process!\n");
         printf(" CHILD: My PID is %d\n", getpid());
@@ -22,9 +13,7 @@ int main(void)
         printf(" CHILD: Enter my exit status (make it small): ");
         scanf(" %d", &rv);
         printf(" CHILD: I'm outta here!\n");
-		excelp("/bin/ls","ls","/",(char*)0);
         exit(rv);
-
     default:
         printf("PARENT: This is the parent process!\n");
         printf("PARENT: My PID is %d\n", getpid());
@@ -34,6 +23,5 @@ int main(void)
         printf("PARENT: My child's exit status is: %d\n", WEXITSTATUS(rv));
         printf("PARENT: I'm outta here!\n");
     }
-
     return 0;
 }
